7137b28378a5ad100546984f7075391f14081313
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-09-04 11:17:07 -0400

    Update CORS Configuration for front-end

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from firebase_admin import credentials
 from dotenv import load_dotenv
 import os
 import json
+from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -15,6 +16,9 @@ def create_app(test_config=None):
     service_account_key_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
     cred = credentials.Certificate(json.loads(service_account_key_json))
     firebase_admin.initialize_app(cred)
+    
+    # Configure CORS to allow requests from React app
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
     
     # Register Blueprints here
     from .routes import npc_bp
