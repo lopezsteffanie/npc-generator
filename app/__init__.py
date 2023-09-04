b6d1c889bcbcb64e1d940616dfe326211d236d73
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-09-04 09:53:34 -0400

    Updated to user Heroku environment variable for Firebase key

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,17 @@ import firebase_admin
 from firebase_admin import credentials
 from dotenv import load_dotenv
 import os
-
-# Define the Firebase app initialization function
-def initialize_firebase_app():
-    service_account_key_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
-    cred = credentials.Certificate(service_account_key_path)
-    firebase_admin.initialize_app(cred)
+import json
 
 def create_app(test_config=None):
     app = Flask(__name__)
     
     # Load environment variables
-    load_dotenv()
+    # load_dotenv()
     
     # Initialize Firebase app
-    service_account_key_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
-    cred = credentials.Certificate(service_account_key_path)
+    service_account_key_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
+    cred = credentials.Certificate(json.loads(service_account_key_json))
     firebase_admin.initialize_app(cred)
     
     # Register Blueprints here
